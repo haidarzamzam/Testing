9dce57f21ccce0c49cc80f3d799cacdf6dbe29f5
Haidar Zamzam <haidar.zamzam99@gmail.com>
2024-12-27 10:48:54 +0700

    testing android

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column {
                         Greeting(
-                            name = "Android",
+                            name = "Android testing",
                             modifier = Modifier.padding(innerPadding)
                         )
                         Text("Testing")
